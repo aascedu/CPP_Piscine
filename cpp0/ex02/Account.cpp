5aea31492ca38105bd2a43ca9f68bf68309f3d06
arthur <arthurascedusnkrs@gmail.com>
2023-10-27 16:51:02 +0200

    Sheesh CPP00/ex02 and some CPP01/ex00

--- cpp0/ex02/Account.cpp
+++ cpp0/ex02/Account.cpp
@@ -1,17 +1,19 @@
 #include "Account.hpp"
 
-static int	_nbAccounts = 0;
-static int	_totalAmount = 0;
-static int	_totalNbDeposits = 0;
-static int	_totalNbWithdrawals = 0;
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
 
 Account::Account(int initial_deposit)
 {
-	static int	_nbAccounts = 10;
-	static int	_totalAmount = initial_deposit;
-	static int	_totalNbDeposits = 1;
-	static int	_totalNbWithdrawals = 0;
-	std::cout << _nbAccounts << _totalAmount << _totalNbDeposits << _totalNbWithdrawals << std::endl;
+	Account::_nbAccounts++;
+	Account::_totalAmount += initial_deposit;
+	Account::_totalNbDeposits++;
+	_accountIndex = Account::_nbAccounts;
+	_amount = initial_deposit;
+	_nbDeposits = 1;
+	_nbWithdrawals = 0;
 }
 
 Account::~Account()
@@ -20,48 +22,54 @@ Account::~Account()
 
 int Account::getNbAccounts(void)
 {
-	return (_nbAccounts);
+	return (Account::_nbAccounts);
 }
 
 int Account::getTotalAmount(void)
 {
-	return (_totalAmount);
+	return (Account::_totalAmount);
 }
 
 int Account::getNbDeposits(void)
 {
-	return (_totalNbDeposits);
+	return (Account::_totalNbDeposits);
 }
 
 int Account::getNbWithdrawals(void)
 {
-	return (_totalNbWithdrawals);
+	return (Account::_totalNbWithdrawals);
 }
 
 void Account::displayAccountsInfos(void)
 {
+	std::cout << Account::_nbAccounts << Account::_totalAmount << Account::_totalNbDeposits << Account::_totalNbWithdrawals << std::endl;
 }
 
 void Account::makeDeposit(int deposit)
 {
 	_amount += deposit;
+	Account::_totalNbDeposits++;
 	_nbDeposits++;
 }
+
 bool Account::makeWithdrawal(int withdrawal)
 {
 	if (_amount > withdrawal)
 	{
 		_amount -= withdrawal;
+		Account::_totalNbWithdrawals++;
 		_nbWithdrawals++;
 		return (true);
 	}
 	return (false);
 }
+
 int Account::checkAmount(void) const
 {
-	return (_totalAmount);
+	return (_amount);
 }
+
 void Account::displayStatus(void) const
 {
-
+	std::cout << _accountIndex << _amount << _nbDeposits << _nbWithdrawals << std::endl;
 }
