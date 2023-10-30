365051822ba87337910327d4d8fd13d00294cdf2
Arthur <arthurascedusnkrs@gmail.com>
2023-10-30 11:26:04 +0100

    Hey

--- cpp1/ex04/openFile.cpp
+++
@@ -1,9 +0,0 @@
-#include "openFile.hpp"
-
-std::fstream	getFileInfo(char *filename)
-{
-	std::fstream	fs;
-
-	fs.open(filename, std::fstream::in);
-	return fs;
-}
--- cpp1/ex04/openFile.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef OPENFILE_HPP
-# define OPENFILE_HPP
-
-# include <fstream>
-# include <iostream>
-
-std::fstream	getFileInfo(char *filename);
-
-#endif
