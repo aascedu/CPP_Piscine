8119a9bbd4da58f870bcf9ced0e2ed2f58073967
Arthur <arthurascedusnkrs@gmail.com>
2023-10-31 08:27:01 +0100

    Bonus ex06

---
+++ cpp1/ex06/Harl.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Harl
+{
+	public:
+		Harl();
+		~Harl();
+		void	complain( std::string level );
+		void debug( void );
+		void info( void );
+		void warning( void );
+		void error( void );
+};
+
+typedef void	(Harl::*functionCaller)(void);
