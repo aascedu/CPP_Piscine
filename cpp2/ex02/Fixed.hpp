93c4a08cb6e7737c5486c66caa15bd777657db1b
Arthur <arthurascedusnkrs@gmail.com>
2023-11-02 09:45:14 +0100

    .

--- cpp2/ex02/Fixed.hpp
+++ cpp2/ex02/Fixed.hpp
@@ -34,9 +34,9 @@ class Fixed
 		Fixed	operator++(int);
 		Fixed	&operator--();
 		Fixed	operator--(int);
-		static Fixed	&min(Fixed &a, Fixed &b);
+		static Fixed		&min(Fixed &a, Fixed &b);
 		static const Fixed	&min(const Fixed &a, const Fixed &b);
-		static Fixed	&max(Fixed &a, Fixed &b);
+		static Fixed		&max(Fixed &a, Fixed &b);
 		static const Fixed	&max(const Fixed &a, const Fixed &b);
 };
 
