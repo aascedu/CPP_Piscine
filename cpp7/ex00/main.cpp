8eea29e15c87769e20116f8b65b4a8ddfad861a2
Arthur Ascedu <aascedu@z4r7p1.42lyon.fr>
2023-11-22 13:21:56 +0100

    Fix main.cpp cpp7/ex00

--- cpp7/ex00/main.cpp
+++ cpp7/ex00/main.cpp
@@ -8,7 +8,17 @@ int	main(void)
 	std::string	b = "chaine2";
 
 	std::cout << ::min(a, b) << std::endl;
+	std::cout << "a: " << a << " b: " << b << std::endl;
 	::swap(a, b);
 	std::cout << "a: " << a << " b: " << b << std::endl;
 	std::cout << ::min(a, b) << std::endl;
+
+	int	c = 1;
+	int	d = 7;
+
+	std::cout << ::min(c, d) << std::endl;
+	std::cout << "c: " << c << " d: " << d << std::endl;
+	::swap(c, d);
+	std::cout << "c: " << c << " d: " << d << std::endl;
+	std::cout << ::max(c, d) << std::endl;
 }
