50479fb2d68f4a74a14480941f07129e0c772c47
Arthur Ascedu <aascedu@z4r8p1.42lyon.fr>
2023-11-29 15:55:22 +0100

    Fix some stuff / finish cpp8

--- cpp8/ex02/MutantStack.hpp
+++ cpp8/ex02/MutantStack.hpp
@@ -1,4 +1,86 @@
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
+# include <iostream>
+# include <stack>
+# include <list>
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+	private:
+	public:
+		MutantStack();
+		~MutantStack();
+		typedef typename std::stack<T>::container_type::iterator			iterator;
+		typedef typename std::stack<T>::container_type::const_iterator		const_iterator;
+		typename std::stack<T>::container_type::iterator		begin();
+		typename std::stack<T>::container_type::const_iterator	cbegin() const;
+		typename std::stack<T>::container_type::iterator		end();
+		typename std::stack<T>::container_type::const_iterator	cend() const;
+		typename std::stack<T>::container_type::iterator		rend();
+		typename std::stack<T>::container_type::const_iterator	crend() const;
+		typename std::stack<T>::container_type::iterator		rbegin();
+		typename std::stack<T>::container_type::const_iterator	crbegin() const;
+};
+
+template<typename T>
+MutantStack<T>::MutantStack()
+{
+}
+
+template<typename T>
+MutantStack<T>::~MutantStack()
+{
+}
+
+template<typename T>
+typename std::stack<T>::container_type::iterator	MutantStack<T>::begin()
+{
+	return (std::stack<T>::c.begin());
+}
+
+template<typename T>
+typename std::stack<T>::container_type::const_iterator	MutantStack<T>::cbegin() const
+{
+	return (std::stack<T>::c.cbegin());
+}
+
+template<typename T>
+typename std::stack<T>::container_type::iterator	MutantStack<T>::end()
+{
+	return (std::stack<T>::c.end());
+}
+
+template<typename T>
+typename std::stack<T>::container_type::const_iterator	MutantStack<T>::cend() const
+{
+	return (std::stack<T>::c.cend());
+}
+
+template<typename T>
+typename std::stack<T>::container_type::iterator	MutantStack<T>::rend()
+{
+	return (std::stack<T>::c.rend());
+}
+
+template<typename T>
+typename std::stack<T>::container_type::const_iterator	MutantStack<T>::crend() const
+{
+	return (std::stack<T>::c.crend());
+}
+
+template<typename T>
+typename std::stack<T>::container_type::iterator	MutantStack<T>::rbegin()
+{
+	return (std::stack<T>::c.rbegin());
+}
+
+template<typename T>
+typename std::stack<T>::container_type::const_iterator	MutantStack<T>::crbegin() const
+{
+	return (std::stack<T>::c.crbegin());
+}
+
+
 #endif
--- cpp8/ex02/main.cpp
+++ cpp8/ex02/main.cpp
@@ -1,1 +1,49 @@
 #include "MutantStack.hpp"
+
+int	main(void)
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+
+	std::list<int> mstack1;
+	mstack1.push_back(5);
+	mstack1.push_back(17);
+	std::cout << mstack1.end() << std::endl;
+	mstack1.pop_back();
+	std::cout << mstack1.size() << std::endl;
+	mstack1.push_back(3);
+	mstack1.push_back(5);
+	mstack1.push_back(737);
+	//[...]
+	mstack1.push_back(0);
+	std::list<int>::iterator it1 = mstack1.begin();
+	std::list<int>::iterator ite1 = mstack1.end();
+	++it1;
+	--it1;
+	while (it1 != ite1)
+	{
+		std::cout << *it1 << std::endl;
+		++it1;
+	}
+	std::list<int> s1(mstack1);
+	return 0;
+}
