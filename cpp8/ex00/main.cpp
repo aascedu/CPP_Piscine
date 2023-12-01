85733488c5da090abfcf0f39c24fd3fce1421426
Arthur Ascedu <aascedu@z4r8p1.42lyon.fr>
2023-12-01 11:11:22 +0100

    ADDED A TEST

--- cpp8/ex00/main.cpp
+++ cpp8/ex00/main.cpp
@@ -4,15 +4,30 @@
 
 int	main(void)
 {
-	std::vector<int> a;
+	{
+		std::vector<int> a;
 
-	a.push_back(4);
-	a.push_back(9999);
-	a.push_back(7);
-	a.push_back(6);
-	a.push_back(2);
+		a.push_back(4);
+		a.push_back(9999);
+		a.push_back(7);
+		a.push_back(6);
+		a.push_back(2);
 
-	std::vector<int>::iterator res = test<std::vector<int>::iterator>(a, 7);
+		std::vector<int>::iterator res = test<std::vector<int>::iterator>(a, 7);
 
-	std::cout << *res << std::endl;
+		std::cout << *res << std::endl;
+	}
+	{
+		std::vector<int> a;
+
+		a.push_back(4);
+		a.push_back(9999);
+		a.push_back(7);
+		a.push_back(6);
+		a.push_back(2);
+
+		std::vector<int>::iterator res = test<std::vector<int>::iterator>(a, 1111111);
+
+		std::cout << *res << std::endl;
+	}
 }
